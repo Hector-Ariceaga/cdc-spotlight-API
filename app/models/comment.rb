3b8c82602b5ffffef6fc5f167ec2ad861d31ee95
Hector-Ariceaga <crablogni@hotmail.com>
2019-02-13 17:56:06 -0800

    Remove validation for a comment's review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
-  validates :content, :review, presence: true
-  validates :review, inclusion: [true, false]
+  validates :content, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context 'validations' do
     it { should validate_presence_of(:content) }
-    it { should validate_presence_of(:review) }
   end
 
   context 'relationships' do
